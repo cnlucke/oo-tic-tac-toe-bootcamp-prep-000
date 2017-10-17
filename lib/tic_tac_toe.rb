ed4378f2e178c4f540cbb10a9d1c8993d2643ac4
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-17 15:48:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,19 @@
 class TicTacToe
+
+
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
+
+    @WIN_COMBINATIONS = [
+      [0,1,2], # Top row
+      [3,4,5],  # Middle row
+      [6,7,8],  # Bottom row
+      [0,3,6],  # First vertical row
+      [1,4,7],  # Second verical row
+      [2,5,8],  # Last vertical row
+      [0,4,8],  # Diagonal from top left
+      [2,4,6]  # Diagonal from bottom left
+    ]
   end
 
   def current_player
@@ -22,29 +35,45 @@ class TicTacToe
   def turn
     puts "Please enter 1-9:"
     input = gets.strip
-    index = input_to_index(input)
-    if(valid_move?(index))
-      move(index, current_player(board))
+    @index = input_to_index(input)
+    if(valid_move?)
+      move(current_player)
       display_board
     else
       turn
     end
   end
 
-  def valid_move?(index)
-    if index.between?(0,8) && !(position_taken?(index))
+  def valid_move?
+    if @index.between?(0,8) && !(position_taken?)
       true
     else
       false
     end
   end
 
-  def position_taken?(index)
-    !(@board[index].nil? || @board[index] == " ")
+  def position_taken?
+    !(@board[@index].nil? || @board[@index] == " ")
   end
 
   def input_to_index(choice)
     return choice.to_i - 1
   end
 
+  def move(current_player)
+    @board[@index] = current_player
+  end
+
+  def play
+    until(over?)
+      turn
+    end
+
+    if(draw?)
+      puts "Cat's Game!"
+    else
+      puts "Congratulations #{winner(board)}!"
+    end
+  end
+  
 end
