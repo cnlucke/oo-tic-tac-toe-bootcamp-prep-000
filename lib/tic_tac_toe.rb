f13f86f20be10cd8c575cd5f130f974c05d60d6a
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-17 15:50:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,4 @@
 class TicTacToe
-
-
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
 
@@ -64,6 +62,45 @@ class TicTacToe
     @board[@index] = current_player
   end
 
+  def won?
+    @WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+      position_1 = @board[win_index_1]
+      position_2 = @board[win_index_2]
+      position_3 = @board[win_index_3]
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X")
+        return win_combination
+      elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return win_combination
+      else
+        false
+      end
+    end
+    false
+  end
+
+  def full?
+  return @board.none? {|position| position == " "}
+end
+
+def draw?
+  if(!won? && full?)
+    return true
+  else
+    return false
+  end
+end
+
+def over?
+  if(full? || draw? || won?)
+    return true
+  else
+    return false
+  end
+end
+
   def play
     until(over?)
       turn
@@ -75,5 +112,5 @@ class TicTacToe
       puts "Congratulations #{winner(board)}!"
     end
   end
-  
+
 end
